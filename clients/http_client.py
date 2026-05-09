ba145f792d9bb1540c49daef65882eebcacb3ae0
Alexey Novarov <alexey.novarov@gmail.com>
2026-05-09 18:03:55 +0300

    http client added to the project

---
+++ clients/http_client.py
@@ -0,0 +1,12 @@
+import requests
+from config.settings import BASE_URL
+
+class HttpClient:
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers.update({"Content-Type": "application/json"})
+        self.base_url = BASE_URL
+
+    def post(self, path: str, payload: dict) -> requests.Response:
+        url = f"{self.base_url}{path}"
+        return self.session.post(url, json=payload)
