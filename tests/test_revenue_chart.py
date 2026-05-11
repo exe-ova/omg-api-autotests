c7bcc099bcdf86095deb76a72da4808362e79461
Alexey Novarov <alexey.novarov@gmail.com>
2026-05-11 15:11:57 +0300

    Revenue Chart case id8 added

--- tests/test_revenue_chart.py
+++ tests/test_revenue_chart.py
@@ -17,3 +17,18 @@ class TestGetRevenueChart:
         adapter = TypeAdapter(list[RevenueChartItem])
         model = adapter.validate_python(response.json())
         assert len(model) > 0
+
+    @qase.id(8)
+    @qase.title("Get Revenue Chart w/ date params - get a requested data")
+    @pytest.mark.smoke
+    def test_get_w_date_params_get_requested(self, dashboard_api):
+        date = "2024-01-04"
+        revenue = 1135.5
+
+        response = dashboard_api.get_revenue_chart(params={"start_date": date, "end_date": date})
+
+        assert response.status_code == 200
+        adapter = TypeAdapter(list[RevenueChartItem])
+        model = adapter.validate_python(response.json())
+        assert model[0].revenue == revenue
+        assert model[0].month == date[:-3]
