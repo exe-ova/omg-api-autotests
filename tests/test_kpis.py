8ef4793c38fa68295680bd5a11906b57dc5b5827
Alexey Novarov <alexey.novarov@gmail.com>
2026-05-11 13:34:47 +0300

    refactor tests: id1 and id2 - pydantic added, asserts reworked

--- tests/test_kpis.py
+++ tests/test_kpis.py
@@ -1,6 +1,8 @@
 import pytest
 from qase.pytest import qase
 from clients.http_client import log_request
+from models.dashboard import KPIsResponse
+
 class TestGetKPIs:
 
     @qase.id(1)
@@ -8,19 +10,27 @@ class TestGetKPIs:
     @pytest.mark.smoke
     def test_get_default_kpis(self, dashboard_api):
         response = dashboard_api.get_kpis()
-        body = response.json()
+
+        model = KPIsResponse(**response.json())
         assert response.status_code == 200
-        assert response.json()["total_orders"] == 1000
-        assert isinstance(body["total_revenue"], float)
-        assert isinstance(body["avg_order_value"], float)
+        assert model.total_orders > 0
+        assert model.total_revenue > 0
+        assert model.avg_order_value > 0
 
     @qase.id(2)
     @qase.title("GET with custom params - get custom response")
     @pytest.mark.smoke
     def test_get_kpis_w_date_params(self, dashboard_api):
-        response = dashboard_api.get_kpis(params={"start_date": "2024-01-01", "end_date": "2024-01-02"})
-        body = response.json()
+        start_date = "2024-01-04"
+        end_date = "2024-01-04"
+        expected_total_orders = 2
+        expected_total_revenue = 1135.5
+        expected_avg_order_value = 567.75
+
+        response = dashboard_api.get_kpis(params={"start_date": start_date, "end_date": end_date})
+
+        model = KPIsResponse(**response.json())
         assert response.status_code == 200
-        assert isinstance(body["total_revenue"], float)
-        assert isinstance(body["total_orders"], int)
-        assert isinstance(body["avg_order_value"], float)
+        assert model.total_orders == expected_total_orders
+        assert model.total_revenue == expected_total_revenue
+        assert model.avg_order_value == expected_avg_order_value
