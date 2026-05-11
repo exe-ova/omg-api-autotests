f8ca802bfbfb94187a22d30982ed81a6e6003df4
Alexey Novarov <alexey.novarov@gmail.com>
2026-05-11 14:06:10 +0300

    added Orders api call

--- api/dashboard.py
+++ api/dashboard.py
@@ -6,3 +6,6 @@ class DashboardApi:
 
     def get_kpis(self, params: dict = None) -> requests.Response:
         return self.client.get("/kpis", params=params)
+
+    def get_orders (self, params: dict = None) -> requests.Response:
+        return self.client.get("/orders", params=params)
