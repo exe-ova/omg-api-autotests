8b1f788b0e9d3b168052b8d4bdb657bfc1c96a4f
Alexey Novarov <alexey.novarov@gmail.com>
2026-05-11 14:34:45 +0300

    case id5 added to the Orders suite

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -20,3 +20,34 @@ class TestGetKPIs:
         assert model.page == page_default
         assert model.per_page == per_page_default
         assert len(model.orders) == per_page_default
+
+    @qase.id(5)
+    @qase.title("Get Orders w/ custom params - get a requested data")
+    @pytest.mark.smoke
+    def test_get_w_custom_params_get_requested_order(self, dashboard_api):
+        page_default = 1
+        per_page_default = 20
+        date = "2024-01-22"
+        status = "cancelled"
+        customer_name = "Laura Bennett"
+        id = 979
+        product = "Collaboration Hub"
+        quantity = 10
+        revenue = 859.5
+
+        response = dashboard_api.get_orders(params={"start_date": date,
+                                                    "end_date": date,
+                                                    "status": status,
+                                                    "search": customer_name,
+                                                    "page_default": page_default,
+                                                    "per_page_default": per_page_default})
+
+        model = OrdersResponse(**response.json())
+        assert response.status_code == 200
+        assert model.page == page_default
+        assert model.per_page == per_page_default
+        assert len(model.orders) == 1
+        assert model.orders[0].id == id
+        assert model.orders[0].product == product
+        assert model.orders[0].quantity == quantity
+        assert model.orders[0].revenue == revenue
